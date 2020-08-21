0c1d85322d1cd3ce54d89fdfdb1496ed8499e193
Dan-RT <daniel.regnard@efrei.net>
2020-08-20 21:24:56 -0400

    Added raspberry code

--- backend/modules/app/controllers/info.py
+++ backend/modules/app/controllers/info.py
@@ -6,6 +6,7 @@ from flask_jwt_extended import (create_access_token, create_refresh_token,
 from app import app, mongo, flask_bcrypt, jwt
 from app.schemas import validate_user
 import logger
+import RPi.GPIO as GPIO
 import time
 
 ROOT_PATH = os.environ.get('ROOT_PATH')
@@ -13,21 +14,47 @@ LOG = logger.get_root_logger(
     __name__, filename=os.path.join(ROOT_PATH, 'output.log'))
 
 
+
+def triggerMachine():
+    servoPIN = 17
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(servoPIN, GPIO.OUT)
+
+    p = GPIO.PWM(servoPIN, 50)  # GPIO 17 for PWM with 50Hz
+    p.start(2.5)  # Initialization
+
+    p.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(12.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(2.5)
+    time.sleep(0.5)
+
+
+
 @app.route('/feed', methods=['GET'])
 @jwt_required
 def feed():
     if request.method == 'GET':
-        print("GET /feed")
+        triggerMachine()
 
         current_user = get_jwt_identity()
 
-        print(current_user["email"])
-
         t = time.localtime()
         current_time = time.strftime("%d %b %Y %Hh%M", t)
-        print(current_time)
 
         food = {'type': "Croquettes", 'date': current_time, 'feeder': current_user["email"]}
+
         mongo.db.food.insert_one(food)
 
         return jsonify({'ok': True, 'data': food}), 200
@@ -35,7 +62,7 @@ def feed():
         mongo.db.drop_collection('food')
         return jsonify({'ok': True}), 200
 
-@app.route('/info', methods=['GET', 'DELETE'])
+@app.route('/info', methods=['GET'])
 #@jwt_required
 def info():
     ''' route read user '''
@@ -45,3 +72,7 @@ def info():
         docs = list(data)
 
         return jsonify({'ok': True, 'data': docs}), 200
+
+
+
+
